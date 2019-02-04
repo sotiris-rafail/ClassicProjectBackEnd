920fe2f308c9f7a546f5e3dc6ae0a5c6a6dc4090
sotiris-rafail <smoschop@gmail.com>
2019-02-04 08:01:33 +0200

    adds classes

--- src/main/java/com/classic/project/model/constantParty/ConstantPartyRepository.java
+++ src/main/java/com/classic/project/model/constantParty/ConstantPartyRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface ConstantPartyRepository extends JpaRepository<ConstantParty, Integer> {
 
-    @Query("select cp from ConstantParty cp, User user where user.cp = cp.cpId and user.userId = ?1")
+    @Query("select cp from ConstantParty cp, User user where user.cp.cpId = cp.cpId and user.userId = ?1")
     Optional<ConstantParty> findByLeaderId(int userId);
 }
