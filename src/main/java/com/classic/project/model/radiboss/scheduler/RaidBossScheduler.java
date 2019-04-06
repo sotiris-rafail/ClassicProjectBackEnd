6cccaf10e149c12d506bc213c18cd3a21eb1aa69
sotiris-rafail <smoschop@gmail.com>
2019-04-06 17:05:33 +0300

    Update RaidBossScheduler.java

--- src/main/java/com/classic/project/model/radiboss/scheduler/RaidBossScheduler.java
+++ src/main/java/com/classic/project/model/radiboss/scheduler/RaidBossScheduler.java
@@ -3,7 +3,6 @@ package com.classic.project.model.radiboss.scheduler;
 import com.classic.project.model.radiboss.*;
 import com.classic.project.model.radiboss.response.ResponseRaidBoss;
 import com.classic.project.model.user.UserRepository;
-import com.sun.deploy.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -26,23 +25,23 @@ public class RaidBossScheduler {
 
     @Scheduled(cron = "*/60 * * * * *") //the top of every hour of every day.
     public void NotifyOnWindow(){
-        List<RaidBoss> bossesOnWindow = new ArrayList<>();
-        List<RaidBoss> epicBosses = raidBossRepository.findAllByTypeOfRaidBoss(TypeOfRaidBoss.EPIC);
+	List<RaidBoss> bossesOnWindow = new ArrayList<>();
+	List<RaidBoss> epicBosses = raidBossRepository.findAllByTypeOfRaidBoss(TypeOfRaidBoss.EPIC);
 	for(RaidBoss raidBoss : epicBosses) {
 	    Calendar deathTimer = Calendar.getInstance();
 	    deathTimer.setTime(raidBoss.getTimeOfDeath());
 	    Date windowStarts = RaidBossServiceImpl.getWindowStarts(deathTimer, raidBoss.getWindowStarts().split(":")).getTime();
 	    Date windowEnds = RaidBossServiceImpl.getWindowStarts(deathTimer, raidBoss.getWindowEnds().split(":")).getTime();
 	    if(!raidBoss.isNotified() && !raidBoss.isUnknown()){
-	        if(ResponseRaidBoss.raidBossState(windowStarts, windowEnds, false).equals(RaidBossState.ONWINDOW.name())){
-			bossesOnWindow.add(raidBoss);
+		if(ResponseRaidBoss.raidBossState(windowStarts, windowEnds, false).equals(RaidBossState.ONWINDOW.name())){
+		    bossesOnWindow.add(raidBoss);
 		}
 	    }
 	}
 	if(!bossesOnWindow.isEmpty()) {
 	    sendMail(bossesOnWindow);
 	    for(RaidBoss raidBoss : bossesOnWindow){
-	        raidBossRepository.updateDeathTimerNotification(raidBoss.getRaidBossId());
+		raidBossRepository.updateDeathTimerNotification(raidBoss.getRaidBossId());
 	    }
 	}
     }
@@ -57,19 +56,21 @@ public class RaidBossScheduler {
 	javaMailSender.send(mail);
     }
 
-    private String getRecipients(List<String> allEmails) {
-	StringBuilder text = new StringBuilder();
-	for(String email : allEmails){
-	    text.append(email).append(",");
+    private static String[] getRecipients(List<String> allEmails) {
+        int i = 0;
+        String recipients[] =  new String[allEmails.size()];
+        for(String email : allEmails) {
+            recipients[i++] = email;
 	}
-        return text.toString().substring(0, text.length() -1);
+	return recipients;
     }
 
+
     private String getText(List<RaidBoss> bossesOnWindow) {
-        StringBuilder text = new StringBuilder();
-        for(RaidBoss raidBoss : bossesOnWindow) {
-            text.append(raidBoss.getName()).append(" is on window now.").append(" Please log your spy.").append(" State changed at ").append(new Date()).append("\n");
+	StringBuilder text = new StringBuilder();
+	for(RaidBoss raidBoss : bossesOnWindow) {
+	    text.append(raidBoss.getName()).append(" is on window now.").append(" Please log your spy.").append(" State changed at ").append(new Date()).append("\n");
 	}
-        return text.toString();
+	return text.toString();
     }
 }
