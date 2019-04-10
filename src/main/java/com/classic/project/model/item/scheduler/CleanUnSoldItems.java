e78f554bfbd2a3a09fcad097c5e8d9a4c530a7c0
sotiris-rafail <smoschop@gmail.com>
2019-04-11 01:08:05 +0300

    Adds mail notification when items are bought or expired

--- src/main/java/com/classic/project/model/item/scheduler/CleanUnSoldItems.java
+++ src/main/java/com/classic/project/model/item/scheduler/CleanUnSoldItems.java
@@ -6,9 +6,12 @@ import com.classic.project.model.item.sold.SoldItemRepository;
 import com.classic.project.model.item.unSold.UnSoldItem;
 import com.classic.project.model.item.unSold.UnSoldItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -21,31 +24,57 @@ public class CleanUnSoldItems {
     @Autowired
     private SoldItemRepository soldItemRepository;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Scheduled(cron = "0 0 * * * *") //the top of every hour of every day.
     public void cleanUpTheSoldUnsoldItem() {
         List<UnSoldItem> unSoldItems = unSoldItemRepository.getSoldUnSoldItemsByStateOfItem();
+	List<SoldItem> soldItems = new ArrayList<>();
 	for(UnSoldItem unSoldItem : unSoldItems){
-	    registerUnSoldItemsAsSoldItems(unSoldItem);
+	    soldItems.add(registerUnSoldItemsAsSoldItems(unSoldItem));
 	    deleteUnSoldItems(unSoldItem.getItemId());
 	}
+	notifyForSoldItems(soldItems);
     }
 
     @Scheduled(cron = "0 0 * * * *") //the top of every hour of every day.
     public void cleanUpExpiredUnSoldItems(){
 	List<UnSoldItem> unSoldItems = unSoldItemRepository.getUnSoldItemsByStateOfItem();
+	List<SoldItem> soldItems = new ArrayList<>();
 	for(UnSoldItem unSoldItem : unSoldItems){
 	    if(unSoldItem.getExpirationDate().before(new Date())){
 	        unSoldItem.setStateOfItem(StateOfItem.SOLD);
-		registerUnSoldItemsAsSoldItems(unSoldItem);
+		soldItems.add(registerUnSoldItemsAsSoldItems(unSoldItem));
 		deleteUnSoldItems(unSoldItem.getItemId());
 	    }
 	}
+	notifyForSoldItems(soldItems);
     }
 
-    private void registerUnSoldItemsAsSoldItems(UnSoldItem unSoldItem) {
+    private void notifyForSoldItems(List<SoldItem> soldItems) {
+	SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+	simpleMailMessage.setFrom("inquisitionalliance@gmail.com");
+	simpleMailMessage.setTo("vasile.nastasiu@gmail.com");
+	simpleMailMessage.setSubject("Item(s) to be delivered");
+	simpleMailMessage.setText(getText(soldItems));
+	simpleMailMessage.setSentDate(new Date());
+	javaMailSender.send(simpleMailMessage);
+    }
+
+    private String getText(List<SoldItem> soldItems){
+        StringBuilder text = new StringBuilder();
+        for(SoldItem soldItem : soldItems){
+            text.append(soldItem.getItemName()).append(" has been sold at the price of ").append(soldItem.getBoughtPrice()).
+		append(" adena.").append("The winner is ").append(soldItem.getWhoBoughtIt()).append(".\n");
+	}
+        return text.toString();
+    }
+
+    private SoldItem registerUnSoldItemsAsSoldItems(UnSoldItem unSoldItem) {
 	SoldItem soldItem = new SoldItem(unSoldItem.getItemId(), unSoldItem.getGrade(), unSoldItem.getItemType(), unSoldItem.getPhotoPath(), unSoldItem.getItemName(), unSoldItem.getStateOfItem(), unSoldItem.getMaxPrice(), unSoldItem.getCurrentValue(), unSoldItem.getLastBidder(), false);
 	soldItem.setRegisterDate(new Date());
-	soldItemRepository.save(soldItem);
+	return soldItemRepository.save(soldItem);
     }
 
     private void deleteUnSoldItems(int itemId){
