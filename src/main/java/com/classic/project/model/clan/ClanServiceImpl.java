cefd947333b225743ca778b093d244e59e933d82
Sotiris <smoschop@gmail.com>
2019-04-12 12:58:42 +0300

    Adds sorting for clan members based on type and level

--- src/main/java/com/classic/project/model/clan/ClanServiceImpl.java
+++ src/main/java/com/classic/project/model/clan/ClanServiceImpl.java
@@ -1,5 +1,6 @@
 package com.classic.project.model.clan;
 
+import com.classic.project.model.character.responce.ResponseCharacter;
 import com.classic.project.model.clan.exception.ClanExistException;
 import com.classic.project.model.clan.responce.ClanResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Component
@@ -20,6 +22,9 @@ public class ClanServiceImpl implements ClanService {
     @Override
     public ResponseEntity<List<ClanResponseEntity>> getAllClanInfo() {
         List<ClanResponseEntity> responseMembers = ClanResponseEntity.convertAll(clanRepository.findAll());
+        for (ClanResponseEntity clan: responseMembers) {
+            clan.getMembers().sort(Comparator.comparing(ResponseCharacter::getTypeOfCharacter).reversed().thenComparing(ResponseCharacter::getLevel));
+        }
         return new ResponseEntity<>(responseMembers, HttpStatus.OK);
     }
 
