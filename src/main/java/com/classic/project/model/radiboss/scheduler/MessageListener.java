c4ea6daa5364490b16f3224059b1ba1081e4b92f
sotiris-rafail <smoschop@gmail.com>
2019-10-07 20:14:34 +0300

    Update MessageListener.java

--- src/main/java/com/classic/project/model/radiboss/scheduler/MessageListener.java
+++ src/main/java/com/classic/project/model/radiboss/scheduler/MessageListener.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Component
-public class MessageListener extends ListenerAdapter implements EventListener {
+public class MessageListener extends ListenerAdapter {
 
     @Autowired
     private RaidBossService raidBossService;
@@ -37,27 +37,29 @@ public class MessageListener extends ListenerAdapter implements EventListener {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         logger.info("LOGGING I RECEIVED THE FOLLOW {} {} {}: {}", event.getGuild().getName(), event.getTextChannel().getName(), Objects.requireNonNull(event.getMember()).getEffectiveName(), event.getMessage().getContentDisplay());
-        try {
             if (event.getMessage().getContentRaw().startsWith("!raidboss") && event.getChannel().getName().equals("general")) {
-                TextChannel textChannel = event.getGuild().getTextChannelsByName("raid_boss_spam", true).get(0);
-                textChannel.sendMessage(buildBossesOutput(raidBossService.getEpicsAndMinisForDiscord())).queue();
+                try {
+		    TextChannel textChannel = event.getGuild().getTextChannelsByName("raid_boss_spam", true).get(0);
+		    textChannel.sendMessage(buildBossesOutput(raidBossService.getEpicsAndMinisForDiscord())).queue();
+		} catch (Exception e) {
+		    TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getChannel().getName(), true).get(0);
+		    textChannel.sendMessage("raid_boss_spam channel is missing").queue();
+		}
             } else if (event.getMessage().getContentRaw().startsWith("!sales") && event.getChannel().getName().equals("general")) {
-                TextChannel textChannel = event.getGuild().getTextChannelsByName("sales_spam", true).get(0);
-                textChannel.sendMessage(buildUnsoldItemsOutput(Objects.requireNonNull(unSoldItemService.getUnSoldItems().getBody()))).queue();
+                try {
+		    TextChannel textChannel = event.getGuild().getTextChannelsByName("sales_spam", true).get(0);
+		    textChannel.sendMessage(
+			buildUnsoldItemsOutput(Objects.requireNonNull(unSoldItemService.getUnSoldItems().getBody())))
+			.queue();
+		}catch (Exception e) {
+		    TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getChannel().getName(), true).get(0);
+		    textChannel.sendMessage("sales_spam channel is missing").queue();
+		}
             }
-        }catch (Exception e) {
-            TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getChannel().getName(), true).get(0);
-            if(event.getMessage().getContentRaw().startsWith("!raidboss")) {
-                textChannel.sendMessage("raid_boss_spam channel is missing").queue();
-            } else if(event.getMessage().getContentRaw().startsWith("!sales")) {
-                textChannel.sendMessage("sales_spam channel is missing").queue();
-            }
-        }
 
     }
-    private List<MessageEmbed.Field> text = new ArrayList<>();
-
     private MessageEmbed buildBossesOutput(List<ResponseRaidBoss> bosses) {
+	List<MessageEmbed.Field> text = new ArrayList<>();
         if(!bosses.isEmpty()) {
             bosses.forEach(boss -> text.add(new MessageEmbed.Field(boss.getName(), boss.toString(), true)));
         }
@@ -66,6 +68,7 @@ public class MessageListener extends ListenerAdapter implements EventListener {
     }
 
     private MessageEmbed buildUnsoldItemsOutput(List<ResponseUnSoldItem> unSoldItems) {
+	List<MessageEmbed.Field> text = new ArrayList<>();
         if(!unSoldItems.isEmpty()) {
             unSoldItems.forEach(unSoldItem -> text.add(new MessageEmbed.Field(unSoldItem.getName(), unSoldItem.toString(), true)));
         }
