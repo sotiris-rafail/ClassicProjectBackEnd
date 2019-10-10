6fc203a0174f97625569a553d8c793506144ae81
Sotiris <smoschop@gmail.com>
2019-10-10 16:20:48 +0300

    Adds 5 minute delay on command channel.

--- src/main/java/com/classic/project/model/radiboss/scheduler/MessageListener.java
+++ src/main/java/com/classic/project/model/radiboss/scheduler/MessageListener.java
@@ -8,6 +8,7 @@ import com.classic.project.model.radiboss.response.ResponseRaidBoss;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.EmbedType;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
@@ -21,9 +22,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
+import java.text.DateFormat;
 import java.time.OffsetDateTime;
 import java.util.*;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.MINUTES;
 
 @Component
 public class MessageListener extends ListenerAdapter {
@@ -49,26 +55,36 @@ public class MessageListener extends ListenerAdapter {
 		logger.info("LOGGING I RECEIVED THE FOLLOW {} {} {}: {}", event.getGuild().getName(), event.getTextChannel().getName(), Objects.requireNonNull(event.getMember()).getEffectiveName(), event.getMessage().getContentDisplay());
 		if(!event.getAuthor().isBot()) {
 			Args args = determineTheArgs(event.getMessage().getContentRaw());
-			if(!event.getMessage().getContentRaw().startsWith("!")) {
+			if (!event.getMessage().getContentRaw().startsWith("!")) {
 				return;
 			}
-			if(isCorrectChannel(args, event)) {
+			if (isCorrectChannel(args, event)) {
 				TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getChannel().getName(), true).get(0);
-				if(event.getGuild().getTextChannelsByName("command_channel", true).size() != 0) {
+				if (event.getGuild().getTextChannelsByName("command_channel", true).size() != 0) {
 					textChannel.sendMessage("Wrong channel. Please switch to <#" + event.getGuild().getTextChannelsByName("command_channel", true).get(0).getId() + ">").queue();
 				} else {
 					textChannel.sendMessage("Missing command_channel chanel. Please create it.").queue();
 				}
 				return;
 			}
+			if (checkForDelay(event)) {
+				TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getChannel().getName(), true).get(0);
+				Date messageTime = new Date(event.getMessage().getTimeCreated().toInstant().toEpochMilli());
+				Calendar displayNextExecutableTIme = Calendar.getInstance();
+				displayNextExecutableTIme.setTime(messageTime);
+				displayNextExecutableTIme.add(Calendar.MINUTE, 5);
+				textChannel.sendMessage(String.format("Previous %s command was executed in less than 5 minutes. Next execution in %2$s", event.getMessage().getContentRaw(),
+						DateFormat.getTimeInstance(1, Locale.getDefault(Locale.Category.DISPLAY)).format(displayNextExecutableTIme.getTime()))).queue();
+				return;
+			}
 			if (args.getCommand().equals("raidboss")) {
 				try {
-					if(TypeOfRaidBoss.getType(args.getType()) != null || args.getType() == null) {
+					if (TypeOfRaidBoss.getType(args.getType()) != null || args.getType() == null) {
 						TextChannel textChannel = event.getGuild().getTextChannelsByName("raid_boss_spam", true).get(0);
 						textChannel.sendMessage(buildBossesOutput(sendRaidBossReply(args), event)).queue();
 					} else {
 						TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getTextChannel().getName(), true).get(0);
-						textChannel.sendMessage(String.format("-type argument can not be '%s'. mini, simple or epic are allowed" , args.getType())).queue();
+						textChannel.sendMessage(String.format("-type argument can not be '%s'. mini, simple or epic are allowed", args.getType())).queue();
 					}
 				} catch (Exception e) {
 					TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getChannel().getName(), true).get(0);
@@ -86,10 +102,37 @@ public class MessageListener extends ListenerAdapter {
 				TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getChannel().getName(), true).get(0);
 				textChannel.sendMessage(buildHelpMessage(event)).queue();
 			} else if (args.getCommand().equalsIgnoreCase("")) {
-                TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getChannel().getName(), true).get(0);
-                textChannel.sendMessage(String.format("Incorrect command %s. Please use !help command and retry.", event.getMessage().getContentRaw())).queue();
-            }
+				TextChannel textChannel = event.getGuild().getTextChannelsByName(event.getChannel().getName(), true).get(0);
+				textChannel.sendMessage(String.format("Incorrect command %s. Please use !help command and retry.", event.getMessage().getContentRaw())).queue();
+			}
 		}
+	}
+
+	private boolean checkForDelay(MessageReceivedEvent event) {
+		final int[] x = {0};
+		long MAX_DURATION = MILLISECONDS.convert(5, MINUTES);
+		List<Message> retrievedHistory = new ArrayList<>();
+		try {
+			retrievedHistory = event.getTextChannel().getHistoryBefore(event.getMessageId(), 100).submit().get().getRetrievedHistory();
+			List<Message> finalRetrievedHistory = retrievedHistory;
+			for (Message message : retrievedHistory) {
+				if (message.getContentRaw().startsWith(event.getMessage().getContentRaw())) {
+					x[0] = finalRetrievedHistory.indexOf(message);
+					break;
+				}
+			}
+			Date messageDateTime = new Date(retrievedHistory.get(x[0]).getTimeCreated().toInstant().toEpochMilli());
+			if(retrievedHistory.get(x[0]).getContentRaw().equalsIgnoreCase(event.getMessage().getContentRaw())) {
+				if ((new Date().getTime() - messageDateTime.getTime()) >= MAX_DURATION) {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			return true;
+		}
+		return true;
 	}
 
 	private static boolean isCorrectChannel(Args args, MessageReceivedEvent event) {
@@ -146,7 +189,7 @@ public class MessageListener extends ListenerAdapter {
 		}
 		return new MessageEmbed("http://83.212.102.61:4200/raidboss", "Information Of Epic/Mini Bosses", "", EmbedType.UNKNOWN, OffsetDateTime.now(),
 				Color.DARK_GRAY.getRGB(), null , new MessageEmbed.Provider("Crusaders Website", "http://83.212.102.61:4200/"), new MessageEmbed.AuthorInfo(event.getJDA().getUserById(580719937119715339L).getName(), BOT_URL, event.getJDA().getUserById(580719937119715339L).getAvatarUrl(), ""), null,
-                new MessageEmbed.Footer(event.getMember() != null ? event.getMember().getEffectiveName() + " asked me about the bosses" : "", event.getMember() != null ? event.getMember().getUser().getAvatarUrl() : "", ""), null, text);
+				new MessageEmbed.Footer(event.getMember() != null ? event.getMember().getEffectiveName() + " asked me about the bosses" : "", event.getMember() != null ? event.getMember().getUser().getAvatarUrl() : "", ""), null, text);
 	}
 
 	private MessageEmbed buildUnsoldItemsOutput(List<ResponseUnSoldItem> unSoldItems, MessageReceivedEvent event) {
@@ -158,7 +201,7 @@ public class MessageListener extends ListenerAdapter {
 		return new MessageEmbed("http://83.212.102.61:4200/auction", "Information Of Auction", "", EmbedType.RICH, OffsetDateTime.now(),
 				Color.DARK_GRAY.getRGB(), null, new MessageEmbed.Provider("Crusaders Website", "http://83.212.102.61:4200/"),
 				new MessageEmbed.AuthorInfo(event.getJDA().getUserById(580719937119715339L).getName(), BOT_URL, event.getJDA().getUserById(580719937119715339L).getAvatarUrl(), ""), null,
-                new MessageEmbed.Footer(event.getMember() != null ? event.getMember().getEffectiveName() + " asked me about the sales" : "", event.getMember() != null ? event.getMember().getUser().getAvatarUrl() : "", ""), null, text);
+				new MessageEmbed.Footer(event.getMember() != null ? event.getMember().getEffectiveName() + " asked me about the sales" : "", event.getMember() != null ? event.getMember().getUser().getAvatarUrl() : "", ""), null, text);
 	}
 
 	private MessageEmbed buildHelpMessage(MessageReceivedEvent event) {
@@ -169,7 +212,7 @@ public class MessageListener extends ListenerAdapter {
 		text.add((new MessageEmbed.Field("Usage", "In order to use the bot three channels are required.\ncommand_channel is used for commanding the bot\nraid_boss_spam is used for displaying the boss results\nsales_spam is used to display all the items on sale.", false)));
 		return new MessageEmbed("", "Help Information", "", EmbedType.RICH, OffsetDateTime.now(),
 				Color.WHITE.getRGB(), null, new MessageEmbed.Provider("Crusaders Website", "http://83.212.102.61:4200/"), new MessageEmbed.AuthorInfo(event.getJDA().getUserById(580719937119715339L).getName(), BOT_URL, event.getJDA().getUserById(580719937119715339L).getAvatarUrl(), ""), null,
-                new MessageEmbed.Footer(event.getMember() != null ? event.getMember().getEffectiveName() + " needed my help." : "", event.getMember() != null ? event.getMember().getUser().getAvatarUrl() : "", ""), null, text);
+				new MessageEmbed.Footer(event.getMember() != null ? event.getMember().getEffectiveName() + " needed my help." : "", event.getMember() != null ? event.getMember().getUser().getAvatarUrl() : "", ""), null, text);
 	}
 
 	public class Args {
